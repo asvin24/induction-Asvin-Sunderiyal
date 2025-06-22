c85727a3c082a2346f2c892840f9f0ded0fd6948
Asvin Sunderiyal <asvinsunderiyal@gmail.com>
2025-06-22 07:19:05 +0530

    Initial commit

--- src/my_robot_controller/my_robot_controller/draw_circle.py
+++ src/my_robot_controller/my_robot_controller/draw_circle.py
@@ -11,10 +11,24 @@ class DrawCircleNode(Node):
         self.timer=self.create_timer(0.5,self.send_velocity_command)
         self.get_logger().info("Draw circle node has been started")
 
+        # Declare parameters with default values
+        self.declare_parameter("linear_x", 2.0)
+        self.declare_parameter("linear_y", 0.0)
+        self.declare_parameter("angular_z", 1.0)
+
+
+
     def send_velocity_command(self):
+
+        #in case we wish to publish new velocity components
+        linear_x = self.get_parameter("linear_x").get_parameter_value().double_value
+        linear_y = self.get_parameter("linear_y").get_parameter_value().double_value
+        angular_z = self.get_parameter("angular_z").get_parameter_value().double_value
+
         msg=Twist()
-        msg.linear.x=2.0
-        msg.angular.z=1.0
+        msg.linear.x=linear_x
+        msg.linear.y=linear_y
+        msg.angular.z=angular_z
         self.cmd_vel_pub_.publish(msg)
 
 def main(args=None):
