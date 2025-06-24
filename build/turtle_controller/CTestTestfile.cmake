162ba02026fd92fd7658d2e90bfc8c4813d9b974
Asvin Sunderiyal <asvinsunderiyal@gmail.com>
2025-06-25 01:37:34 +0530

    Re-adding updated project after reset

---
+++ build/turtle_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/asvin/projectKratos/src/turtle_controller
+# Build directory: /home/asvin/projectKratos/build/turtle_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
