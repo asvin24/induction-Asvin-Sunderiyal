de1efaaf92d7ba2804fba06b3c3ece1a75291e5e
Asvin Sunderiyal <asvinsunderiyal@gmail.com>
2025-06-26 13:01:18 +0530

    package completed

--- obstacle_avoider/scripts/obstacle_avoider.py
+++ obstacle_avoider/scripts/obstacle_avoider.py
@@ -1,5 +1,5 @@
-
 #!/usr/bin/env python3
+
 
 import rclpy
 from rclpy.node import Node
@@ -17,14 +17,39 @@ class ObstacleAvoider(Node):
 
         self.get_logger().info("Obstacle Avoider node started")
 
-        self.linear_speed = 0.2
-        self.angular_speed = -0.5
+        self.vision=15
+
+        self.linear_speed = 1.5
+        self.angular_speed = -7.5
         self.obstacle_distance_threshold = 0.5  # meters
 
-        self.timer = self.create_timer(0.1, self.control_loop)
+        self.timer = self.create_timer(0.1, self.set_velocity)
         self.obstacle_in_front = False
 
-    def scan_callback(self,msg):
+    def scan_callback(self,msg:LaserScan):
+
+        ''' 
+        Callback function receives a LaserScan message (`msg`) which contains ranges
+        a list of distance measurements from
+        representing the distance from the robot to obstacles at different angles.
+        The index in anges corresponds to a float angle in radians times 100
+        starting from `angle_min` incremented by `angle_increment` for each step.
+        '''
+
+        distance_ranges = msg.ranges[-15:]+msg.ranges[:15]
+
+        # Filter out invalid ranges (NaN or inf)
+        distance_ranges= [r for r in distance_ranges if r > 0.0 and r < float('inf')]
+
+        if not distance_ranges:
+            self.get_logger().warn("No valid LaserScan data in front!")
+            self.obstacle_in_front = False
+            return
+        else:
+            self.get_logger().info(f"Min distance ahead: {min(distance_ranges):.2f}")
+            self.obstacle_in_front = min(distance_ranges) < self.obstacle_distance_threshold
+      
+
         pass
 
     def set_velocity(self):
@@ -33,7 +58,7 @@ class ObstacleAvoider(Node):
             twist.linear.x = 0.0
             twist.angular.z = self.angular_speed
         else:
-            twist.linear.x = self.forward_speed
+            twist.linear.x = self.linear_speed
             twist.angular.z = 0.0
 
         self.cmd_vel_pub.publish(twist)
